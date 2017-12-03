0894dbbb55be5f609891e7f102238d6ff4334e47
rsquaredin <rsquaredin@gmail.com>
2017-12-03 22:58:31 +0530

    rename function for launching shiny app

---
+++ R/infer-launch-shiny-app.R
@@ -0,0 +1,23 @@
+#' @importFrom shiny runApp
+#' @title Launch Shiny App
+#' @description Launches shiny app
+#' @examples
+#' \dontrun{
+#' infer_launch_shiny_app()
+#' }
+#' @export
+#'
+infer_launch_shiny_app <- function() {
+    shiny::runApp(appDir = system.file("application", package = "inferr"))
+}
+
+#' @export
+#' @rdname infer_launch_shiny_app
+#' @usage NULL
+#'
+launch_inferr <- function() {
+
+    .Deprecated("infer_launch_shiny_app()")
+    infer_launch_shiny_app(data)
+
+}
