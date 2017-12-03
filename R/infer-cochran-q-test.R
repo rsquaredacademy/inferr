eb5dc1ee409354ac3163b3152e8420759fa85eff
rsquaredin <rsquaredin@gmail.com>
2017-12-03 22:55:17 +0530

    deprecate cochran's q test

--- R/infer-cochran-q-test.R
+++ R/infer-cochran-q-test.R
@@ -3,28 +3,31 @@
 #' equal in the same population.
 #' @param x a data frame/vector
 #' @param ... numeric vectors
-#' @return \code{cochran_test} returns an object of class \code{"cochran_test"}.
-#' An object of class \code{"cochran_test"} is a list containing the
-#' following components:
+#' @return \code{infer_cochran_qtest} returns an object of class
+#' \code{"infer_cochran_qtest"}. An object of class \code{"infer_cochran_qtest"}
+#' is a list containing the following components:
 #'
 #' \item{n}{number of observations}
 #' \item{df}{degrees of freedom}
 #' \item{q}{cochran's q statistic}
 #' \item{pvalue}{p-value}
+#' @section Deprecated Function:
+#' \code{cochran_test()} has been deprecated. Instead use
+#' \code{infer_cochran_qtest()}.
 #' @references Sheskin, D. J. 2007. Handbook of Parametric and Nonparametric
 #' Statistical Procedures, 4th edition. : Chapman & Hall/CRC.
 #'
 #' @examples
-#' cochran_test(exam)
+#' infer_cochran_qtest(exam)
 #' @export
 #'
-cochran_test <- function(x, ...) UseMethod('cochran_test')
+infer_cochran_qtest <- function(x, ...) UseMethod('infer_cochran_qtest')
 
 #' @export
-cochran_test.default <- function(x, ...) {
+infer_cochran_qtest.default <- function(x, ...) {
 
 	data <- coch_data(x, ...)
-	
+
 	if (ncol(data) < 3) {
 		stop('Please specify at least 3 variables.')
 	}
@@ -33,15 +36,26 @@ cochran_test.default <- function(x, ...) {
 		stop('Please specify dichotomous/binary variables only.')
 	}
 
-	k <- cochran_comp(data)		
+	k <- cochran_comp(data)
 	result <- list(n = k$n, df = k$df, q = k$q, pvalue = k$pvalue)
-	class(result) <- 'cochran_test'
+	class(result) <- 'infer_cochran_qtest'
 	return(result)
 
 }
 
 #' @export
+#' @rdname infer_cochran_qtest
+#' @usage NULL
 #'
-print.cochran_test <- function(x, ...) {
+cochran_test <- function(x, ...) {
+
+    .Deprecated("infer_cochran_qtest()")
+    infer_cochran_qtest(x, ...)
+
+}
+
+#' @export
+#'
+print.infer_cochran_qtest <- function(x, ...) {
 	print_cochran_test(x)
 }
--- tests/testthat/test-cochran.R
+++ tests/testthat/test-cochran.R
@@ -1,15 +1,15 @@
 context('cochran')
 
-test_that('output from cochran_test matches the expected result', {
-    k <- cochran_test(exam)
+test_that('output from infer_cochran_qtest matches the expected result', {
+    k <- infer_cochran_qtest(exam)
     expect_equal(k$n, 15)
     expect_equal(k$df, 2)
     expect_equal(k$q, 4.75)
     expect_equal(k$pvalue, 0.093)
 })
 
-test_that('output from cochran_test matches the expected result', {
-    k <- cochran_test(exam$exam1, exam$exam2, exam$exam3)
+test_that('output from infer_cochran_qtest matches the expected result', {
+    k <- infer_cochran_qtest(exam$exam1, exam$exam2, exam$exam3)
     expect_equal(k$n, 15)
     expect_equal(k$df, 2)
     expect_equal(k$q, 4.75)
@@ -17,29 +17,29 @@ test_that('output from cochran_test matches the expected result', {
 })
 
 
-test_that('cochran_test throws appropriate errors', {
-    expect_error(cochran_test(exam$exam1, exam$exam2),
+test_that('infer_cochran_qtest throws appropriate errors', {
+    expect_error(infer_cochran_qtest(exam$exam1, exam$exam2),
                  'Please specify at least 3 variables.')
-    expect_error(cochran_test(exam[, c(1, 2)]),
+    expect_error(infer_cochran_qtest(exam[, c(1, 2)]),
                  'Please specify at least 3 variables.')
-    expect_error(cochran_test(exam$exam1, exam$exam2),
+    expect_error(infer_cochran_qtest(exam$exam1, exam$exam2),
                  'Please specify at least 3 variables.')
-    expect_error(cochran_test(hsb[, c(2, 3, 5)]),
+    expect_error(infer_cochran_qtest(hsb[, c(2, 3, 5)]),
                  'Please specify dichotomous/binary variables only.')
-    expect_error(cochran_test(hsb$female, hsb$schtyp, hsb$race),
+    expect_error(infer_cochran_qtest(hsb$female, hsb$schtyp, hsb$race),
                  'Please specify dichotomous/binary variables only.')
 })
 
 test_that('output from cochran test is as expected', {
 
-  x <- cat("   Test Statistics     
+  x <- cat("   Test Statistics
 ----------------------
-N                   15 
-Cochran's Q       4.75 
-df                   2 
-p value          0.093 
+N                   15
+Cochran's Q       4.75
+df                   2
+p value          0.093
 ----------------------")
 
-  expect_equivalent(print(cochran_test(exam)), x)
+  expect_equivalent(print(infer_cochran_qtest(exam)), x)
 
 })
