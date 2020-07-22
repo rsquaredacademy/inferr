c6e532a04de222343ae9f1c4b9e8ec0be5ffddf8
rsquaredin <rsquaredin@gmail.com>
2020-07-22 16:41:51 +0530

    clean up api

--- R/infer-output.R
+++ R/infer-output.R
@@ -2,26 +2,26 @@ print_owanova <- function(data) {
 
   # width
   w1 <- nchar("Between Groups")
-  w2 <- max(nchar("Squares"), nchar(data$between), nchar(data$within), nchar(data$total))
+  w2 <- max(nchar("Squares"), nchar(data$ss_between), nchar(data$ss_within), nchar(data$ss_total))
   w3 <- max(nchar("DF"), nchar(data$df_btw), nchar(data$df_btw), nchar(data$df_within), nchar(data$df_total))
   w4 <- max(nchar("Mean Square"), nchar(data$ms_btw), nchar(data$ms_within))
-  w5 <- max(nchar("F"), nchar(data$f))
-  w6 <- max(nchar("Sig."), nchar(format(data$sig, nsmall = 4)))
+  w5 <- max(nchar("F"), nchar(data$fstat))
+  w6 <- max(nchar("Sig."), nchar(format(data$pval, nsmall = 4)))
   w <- sum(w1, w2, w3, w4, w5, w6, 21)
-  w7 <- nchar(data$sigma)
+  w7 <- nchar(data$rmse)
 
-  dc <- as.vector(data$tab[, 1])
+  dc <- as.vector(data$group_stats[, 1])
 
   w8 <- max(nchar("Category"), max(nchar(dc)))
-  w9 <- max(nchar("N"), max(nchar(data$tab[[2]])))
-  w10 <- max(nchar("Mean"), max(nchar(format(data$tab[[3]], nsmall = 3))))
-  w11 <- max(nchar("Std. Dev."), max(nchar(format(data$tab[[4]], nsmall = 3))))
+  w9 <- max(nchar("N"), max(nchar(data$group_stats[[2]])))
+  w10 <- max(nchar("Mean"), max(nchar(format(data$group_stats[[3]], nsmall = 3))))
+  w11 <- max(nchar("Std. Dev."), max(nchar(format(data$group_stats[[4]], nsmall = 3))))
   wr <- sum(w8, w9, w10, w11, 13)
 
 
-  p <- format(data$p, nsmall = 4)
-  q <- nrow(data$tab)
-  s <- length(data$tab)
+  p <- format(data$pval, nsmall = 4)
+  q <- nrow(data$group_stats)
+  s <- length(data$group_stats)
 
 
   cat(fg("ANOVA", w), "\n")
@@ -29,9 +29,9 @@ print_owanova <- function(data) {
   cat(fg("", w1), fs(), fg("Sum of", w2), fs(), fg("", w3), fs(), fg("", w4), fs(), fg("", w5), fs(), fg("", w6), "\n")
   cat(fg("", w1), fs(), fg("Squares", w2), fs(), fg("DF", w3), fs(), fg("Mean Square", w4), fs(), fg("F", w5), fs(), fg("Sig.", w6), "\n")
   cat(rep("-", w), sep = "", "\n")
-  cat(fl("Between Groups", w1), fs(), fg(data$between, w2), fs(), fg(data$df_btw, w3), fs(), fg(data$ms_btw, w4), fs(), fg(data$f, w5), fs(), fg(p, w6), "\n")
-  cat(fl("Within Groups", w1), fs(), fg(data$within, w2), fs(), fg(data$df_within, w3), fs(), fg(data$ms_within, w4), fs(), fg("", w5), fs(), fg("", w6), "\n")
-  cat(fl("Total", w1), fs(), fg(data$total, w2), fs(), fg(data$df_total, w3), fs(), fg("", w4), fs(), fg("", w5), fs(), fg("", w6), "\n")
+  cat(fl("Between Groups", w1), fs(), fg(data$ss_between, w2), fs(), fg(data$df_btw, w3), fs(), fg(data$ms_btw, w4), fs(), fg(data$fstat, w5), fs(), fg(data$pval, w6), "\n")
+  cat(fl("Within Groups", w1), fs(), fg(data$ss_within, w2), fs(), fg(data$df_within, w3), fs(), fg(data$ms_within, w4), fs(), fg("", w5), fs(), fg("", w6), "\n")
+  cat(fl("Total", w1), fs(), fg(data$ss_total, w2), fs(), fg(data$df_total, w3), fs(), fg("", w4), fs(), fg("", w5), fs(), fg("", w6), "\n")
   cat(rep("-", w), sep = "", "\n\n")
 
   cat(fg("Report", wr), "\n")
@@ -40,14 +40,14 @@ print_owanova <- function(data) {
   cat(rep("-", wr), sep = "", "\n")
   for (i in seq_len(q)) {
     cat(
-      fc(data$tab[[i, 1]], w8), fs(), fg(data$tab[[i, 2]], w9), fs(), fk(format(round(data$tab[[i, 3]], 3), nsmall = 3), w10),
-      fs(), fk(format(round(data$tab[[i, 4]], 3), nsmall = 3), w11), "\n"
+      fc(data$group_stats[[i, 1]], w8), fs(), fg(data$group_stats[[i, 2]], w9), fs(), fk(format(round(data$group_stats[[i, 3]], 3), nsmall = 3), w10),
+      fs(), fk(format(round(data$group_stats[[i, 4]], 3), nsmall = 3), w11), "\n"
     )
   }
   cat(rep("-", wr), sep = "", "\n\n")
 
   cat(fl("Number of obs", 13), "=", fl(data$obs, w7), fs(), fl("R-squared", 13), "=", data$r2, "\n")
-  cat(fl("Root MSE", 13), "=", data$sigma, fs(), fl("Adj R-squared", 13), "=", data$ar2, "\n\n")
+  cat(fl("Root MSE", 13), "=", data$rmse, fs(), fl("Adj R-squared", 13), "=", data$adjusted_r2, "\n\n")
 }
 
 
